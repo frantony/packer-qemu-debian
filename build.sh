4c8baf3a4f7483bdf2435ca9d0670f1c6948479a
Antony Pavlov <antonynpavlov@gmail.com>
2018-11-06 21:46:29 +0300

    switch to Debian 9 (stretch)
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@
 export PACKER_LOG="yes"
 export PACKER_LOG_PATH="build_output.log"
 
-packer build jessie.json
+packer build stretch.json
 
 if [ $? -eq 0 ]
 then
 
   # Cleanup
   echo "Size before reclaiming free space:"
-  du -sh output-jessie/packer-jessie.qcow2
+  du -sh output-stretch/packer-stretch
   echo "Reclaiming free space..."
-  time qemu-img convert -c -O qcow2 output-jessie/packer-jessie.qcow2 output-jessie/packer-jessie-shrunk.qcow2
+  time qemu-img convert -c -O qcow2 output-stretch/packer-stretch output-stretch/packer-stretch-shrunk.qcow2
   echo "Size after reclaiming free space:"
-  du -sh output-jessie/packer-jessie-shrunk.qcow2
-  rm -f output-jessie/packer-jessie.qcow2
+  du -sh output-stretch/packer-stretch-shrunk.qcow2
+  rm -f output-stretch/packer-stretch
 
 else
     echo "Build failed!"
