f9fd3ee6abbae8ac1d516835ae8c4af99e923079
Antony Pavlov <antonynpavlov@gmail.com>
2018-11-06 21:45:05 +0300

    drop zram stuff
    Starting at buster (current testing) it's possible to simply install the
    zram-tools package and customize /etc/default/zramswap for your needs.
    See https://wiki.debian.org/ZRam for details.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- scripts/tuning.sh
+++ scripts/tuning.sh
@@ -2,8 +2,3 @@
 
 # Set deadline IO scheduler for non-rotating disks
 echo '"ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="deadline"' >> /etc/udev/rules.d/60-schedulers.rules
-
-# Enable ZRAM
-mv /home/packer/zramst* /usr/local/bin/
-mv zram.service /etc/system/zram.service
-systemctl enable zram && systemctl start zram
